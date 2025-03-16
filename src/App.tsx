0f46a85ec797af42fc495b4fd63e476e62f866a5
Brisstone <okolijohnson69@gmail.com>
2025-03-16 12:51:22 +0000

    updtes

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
         <Route path="/super-admin">
           <Route path="" element={<SuperAdminLayout />}>
             <Route index element={<Company />} />
-          </Route>
+          </Route> 
         </Route>
       </Routes>
     </div>
