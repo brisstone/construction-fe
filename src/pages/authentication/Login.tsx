62b6f250d51650ff59624b618b9d11234190411c
Brisstone <okolijohnson69@gmail.com>
2025-03-18 14:46:13 +0000

    deploy

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -42,7 +42,7 @@ const Login = () => {
   const onSubmit = async (data: any) => {
     const response = {
       email: data?.email,
-      primaryColor: "red",
+      primaryColor: "purple",
     };
     try {
       if (data) {
