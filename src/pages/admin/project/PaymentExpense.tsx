7cec1ec3aa690402f9bad7a4d381d3b7db8a9f10
Brisstone <okolijohnson69@gmail.com>
2025-04-04 11:42:47 +0100

    ui-icons

---
+++ src/pages/admin/project/PaymentExpense.tsx
@@ -0,0 +1,107 @@
+import ButtonComp from "@/components/general/ButtonComp";
+import FilterLayout from "@/components/general/FilterLayout";
+import Pagination from "@/components/general/Pagination";
+import ReusableDialog from "@/components/general/ReuseableDialog";
+import RouteChain from "@/components/general/RouteChain";
+import Container from "@/components/layout/Container";
+import NewPaymentModal from "@/components/projects/paymentSchedule/NewPaymentModal";
+import PaymentTable from "@/components/projects/paymentSchedule/PaymentTable";
+import usegetProjectById from "@/hooks/api/queries/projects/getProjectById";
+import useGetPaymentSchedule, {
+  PaymentScheduleType,
+} from "@/hooks/api/queries/projects/paymentSchedule/getPaymentSchedule";
+import { formatNumberWithCommaDecimal, PageTypes } from "@/utils";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+const PaymentExpense = () => {
+  const pageKey = PageTypes.PROJECTS;
+  const { id } = useParams();
+  const { data: project } = usegetProjectById(id ?? "");
+
+  const [openSchedule, setOpenSchedule] = useState(false);
+
+  const [editSchedule, setEditSchedule] = useState<PaymentScheduleType | null>(
+    null
+  );
+
+  const { data: paymentSchedule } = useGetPaymentSchedule(id ?? "");
+
+  const handleModalClose = () => {
+    setOpenSchedule(false);
+    setEditSchedule(null);
+  };
+
+  const handleEdit = (property: PaymentScheduleType) => {
+    setEditSchedule(property);
+    setOpenSchedule(true);
+  };
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <RouteChain
+          routeOne="Projects"
+          routeTwo={`${project?.name}`}
+          routeThree="Payment Schedule"
+        />
+      </div>
+      <Container className="my-5">
+        <aside className="sm:flex items-center justify-between">
+          <p className="font-medium sm:text-lg text-sm text-textShade">
+            Expense Report
+          </p>
+        </aside>
+        <FilterLayout pageKey={pageKey} />
+        <PaymentTable
+          onEdit={handleEdit}
+          paymentSchedule={paymentSchedule?.data ?? []}
+        />
+        <Pagination />
+
+        <div className="p-4 mt-4 flex justify-between">
+          <p className="font-semibold">Total</p>
+          <p></p>
+          <p className="font-semibold -ml-4">
+            {formatNumberWithCommaDecimal(800)}
+          </p>
+        </div>
+
+        <div className="flex justify-between">
+          <ButtonComp
+            onClick={() => setOpenSchedule(true)}
+            text="Save as pdf"
+            className="w-full mt-1 sm:mt-0"
+          />
+          <ButtonComp
+            onClick={() => setOpenSchedule(true)}
+            text="Print Report"
+            className="w-full mt-1 sm:mt-0"
+          />
+        </div>
+        {
+          <ReusableDialog
+            title={
+              editSchedule
+                ? "Edit Payment Schedule"
+                : "Add New Payment Schedule"
+            }
+            open={openSchedule}
+            onOpenChange={setOpenSchedule}
+            className="sm:max-w-[60vw]"
+          >
+            <div>
+              <NewPaymentModal
+                defaultValues={editSchedule || undefined}
+                isEditMode={!!editSchedule}
+                handleModalClose={handleModalClose}
+              />
+            </div>
+          </ReusableDialog>
+        }
+      </Container>
+    </div>
+  );
+};
+
+export default PaymentExpense;
